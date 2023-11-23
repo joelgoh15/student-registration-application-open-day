8493adb170c5cc65a339dc6673ca44f2f3264356
Joel Goh Chong Hui <147377225+joelgoh15@users.noreply.github.com>
2023-11-23 14:23:24 +0800

    Update Admin.aspx.cs

--- AdminPage/Admin.aspx.cs
+++ AdminPage/Admin.aspx.cs
@@ -9,26 +9,15 @@ namespace Portfolio1_1
 {
     public partial class Admin : System.Web.UI.Page
     {
-
-
-
         private void BindGrid()
         {
             using (StudentRegistrationAppDataClassesDataContext studentRegistrationAppDataClassesDataContext = new StudentRegistrationAppDataClassesDataContext())
             {
                 GridView1.DataSource = from stdntRegistrationAppTable in studentRegistrationAppDataClassesDataContext.StudentRegistationAppTables
                                        select stdntRegistrationAppTable;
-
                 GridView1.DataBind();
             }
         }
-
-
-        
-
-
-
-
 
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -38,28 +27,18 @@ namespace Portfolio1_1
             }
         }
 
-
-
-
         protected void GridView1_RowDataBound(object sender, GridViewRowEventArgs e)
         {
-           
-            //highlight on hover
             if (e.Row.RowType == DataControlRowType.DataRow)
             {
                 e.Row.Attributes.Add("onmouseover", "this.style.backgroundColor='lightgray'");
                 e.Row.Attributes.Add("onmouseout", "this.style.backgroundColor='white'");
             }
-
-
-            //confirmation message before delete
             if (e.Row.RowType == DataControlRowType.DataRow && e.Row.RowIndex != GridView1.EditIndex)
             {
                 (e.Row.Cells[7].Controls[2] as LinkButton).Attributes["onclick"] = "return confirm('Do you want to delete this row?');";
             }
-
         }
-
 
         protected void OnRowEditing(object sender, GridViewEditEventArgs e)
         {
@@ -67,41 +46,32 @@ namespace Portfolio1_1
             this.BindGrid();
         }
 
-
         protected void OnRowUpdating(object sender, GridViewUpdateEventArgs e)
         {
             GridViewRow row = GridView1.Rows[e.RowIndex];
             int studentId = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Values[0]);
-
             string strName = (row.FindControl("txtName") as TextBox).Text;
             string strEmail = (row.FindControl("txtEmail") as TextBox).Text;
             string strContactNo = (row.FindControl("txtContactNo") as TextBox).Text;
             string strAddress = (row.FindControl("txtAddress") as TextBox).Text;
             string strAttendDay = (row.FindControl("txtAttendDay") as TextBox).Text;
             string strCourses = (row.FindControl("txtCourses") as TextBox).Text;
-
             using (StudentRegistrationAppDataClassesDataContext studentRegistrationAppDataClassesDataContext = new StudentRegistrationAppDataClassesDataContext())
             { 
                 StudentRegistationAppTable studentRegistationAppTable = (from c in studentRegistrationAppDataClassesDataContext.StudentRegistationAppTables
                                                                          where c.Id == studentId
                                                                          select c).FirstOrDefault();
-
                 studentRegistationAppTable.Name = strName;
                 studentRegistationAppTable.Email = strEmail;
                 studentRegistationAppTable.ContactNo = strContactNo;
                 studentRegistationAppTable.Address = strAddress;    
                 studentRegistationAppTable.AttendDay = strAttendDay;
                 studentRegistationAppTable.Courses = strCourses;
-
                 studentRegistrationAppDataClassesDataContext.SubmitChanges();
-
             }
-
             GridView1.EditIndex = -1;
             this.BindGrid();
-
         }
-
 
         protected void OnRowCancelingEdit(object sender, EventArgs e)
         {
@@ -113,16 +83,13 @@ namespace Portfolio1_1
         protected void OnRowDeleting(object sender, GridViewDeleteEventArgs e)
         {
             int studentId = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Values[0]);
-
             using (StudentRegistrationAppDataClassesDataContext studentRegistrationAppDataClassesDataContext = new StudentRegistrationAppDataClassesDataContext())
             {
                 StudentRegistationAppTable studentRegistationAppTable = (from c in studentRegistrationAppDataClassesDataContext.StudentRegistationAppTables
                                                                          where c.Id == studentId
                                                                          select c).FirstOrDefault();
-
                 studentRegistrationAppDataClassesDataContext.StudentRegistationAppTables.DeleteOnSubmit(studentRegistationAppTable);
                 studentRegistrationAppDataClassesDataContext.SubmitChanges();
-
             }
             this.BindGrid();
         }
@@ -130,14 +97,7 @@ namespace Portfolio1_1
         protected void GridView1_PageIndexChanging(object sender, GridViewPageEventArgs e)
         {
             GridView1.PageIndex = e.NewPageIndex;
-            //GridView1.DataBind();
             this.BindGrid();
-
         }
-
-
-
-
-
-    }//end-class
-}//end-namespace
+    }
+}
